28f458e85c0dfa4875ce9984015269644ab63089
Beck <b.tadesse.77@gmail.com>
2023-07-18 22:04:56 +0300

    Cleanup build scripts

--- scripts/gofmtcheck.sh
+++ scripts/gofmtcheck.sh
@@ -2,18 +2,16 @@
 
 set -euo pipefail
 
-source ./scripts/lib/ui.sh
-
 # Check go fmt
-ui_print_title "Checking that code complies with go fmt requirements..."
+echo "Checking that code complies with go fmt requirements..."
 gofmt_files=$(go fmt ./...)
 if [[ -n ${gofmt_files} ]]; then
-	ui_print_title "Go format check failed"
-	ui_print_content 'gofmt needs running on the following files:' 1
-	ui_print_content "${gofmt_files}" 2
-	ui_print_content "You can use the command: \`go fmt\` to reformat code." 1
+	echo "Go format check failed"
+	echo '    gofmt needs running on the following files:'
+	echo "    ${gofmt_files}"
+	echo "    You can use the command: \`go fmt\` to reformat code."
 	exit 1
 fi
 
-ui_print_content "Done" 1
+echo "Done"
 exit 0
--- scripts/staticcheck.sh
+++ scripts/staticcheck.sh
@@ -2,12 +2,10 @@
 
 set -euo pipefail
 
-source ./scripts/lib/ui.sh
-
-ui_print_title "Checking that code complies with static analysis requirements..."
+echo "Checking that code complies with static analysis requirements..."
 
 packages=$(go list ./...)
 
 go run honnef.co/go/tools/cmd/staticcheck -checks ${packages}
 
-ui_print_content "Done" 1
+echo "Done"
