74c7263646af1cdd97b912ed639affba0295653f
Beck <b.tadesse.77@gmail.com>
2023-08-06 21:31:59 +0300

    Convert `database.GetInstance()` to a singleton
    We don't need multiple `db` objects

--- internal/pkg/database/connection.go
+++ internal/pkg/database/connection.go
@@ -7,11 +7,19 @@ import (
 
 const dsn = "host=localhost user=postgres password=g7bXjBroyyCXyL92ZJT4mQu6hc3pTQAA port=5433 sslmode=disable"
 
+var db *gorm.DB
+
 func GetInstance() (*gorm.DB, error) {
-	var db *gorm.DB
+	if db != nil {
+		return db, nil
+	}
+
 	var err error
 
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		db = nil
+	}
 
 	return db, err
 }
